1cbc8cd5649c879192fd62451466b2c586e67c9d
raj patel <kakhilesh79@gmail.com>
2020-09-17 13:16:28 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+   final questions = const [
+    {
+      'questionText': 'fav color ?',
+      'answers': ['black', 'red', 'green', 'white'],
+    },
+    {
+      'questionText': 'fav animal ?',
+      'answers': ['rabbit', 'snake', 'elephant', 'lion'],
+    },
+  ];
   var _questionIndex = 0;
+
   void _answerQuestion() {
+    if ( _questionIndex < questions.length ) {
     setState(() {
       _questionIndex += 1;
     });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'fav color ?',
-        'answers': ['black', 'red', 'green', 'white'],
-      },
-      {
-        'questionText': 'fav animal ?',
-        'answers': ['rabbit', 'snake', 'elephant', 'lion'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text('My First App'),
         ),
-        body: Column(
+        body: _questionIndex < questions.length  ? Column(
           children: [
             Text(
               questions[_questionIndex]['questionText'],
@@ -45,10 +47,13 @@ class _MyAppState extends State<MyApp> {
                 fontSize: 30,
               ),
             ),
-           ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-             return Answer(_answerQuestion,answer);
-           }).toList()
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
+        ):Center(
+          child: Text('done'),
         ),
       ),
     );
