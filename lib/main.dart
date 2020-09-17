cc719395ac75da22278b42b48efe6fcb045e42af
raj patel <kakhilesh79@gmail.com>
2020-09-17 10:54:41 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/answer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-   
+    var questions = [
+      {
+        'questionText': 'fav color ?',
+        'answers': ['black', 'red', 'green', 'white'],
+      },
+      {
+        'questionText': 'fav animal ?',
+        'answers': ['rabbit', 'snake', 'elephant', 'lion'],
+      },
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -16,7 +39,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-
+            Text(
+              questions[_questionIndex]['questionText'],
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+           ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+             return Answer(_answerQuestion,answer);
+           }).toList()
           ],
         ),
       ),
